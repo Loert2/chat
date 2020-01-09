2a558d324eaa1710eac1210dd1957ab7a347a0b9
Loert2 <den.schopyrev@yandex.ru>
2020-01-09 13:01:42 +0300

    Забытый store

---
+++ src/main/web/src/store/userList/reducer.js
@@ -0,0 +1,27 @@
+import { handleActions } from "redux-actions";
+import { user } from "./actions";
+
+const initialState = {
+  user: {},
+  userList: [],
+  error: null,
+  loading: false
+};
+
+export default handleActions(
+  {
+    [user.list.request]: state => ({
+      ...state,
+      loading: true
+    }),
+    [user.list.success]: (state, action) => ({
+      ...initialState,
+      userList: action.payload
+    }),
+    [user.list.error]: (state, action) => ({
+      ...initialState,
+      error: action.payload
+    })
+  },
+  initialState
+);
